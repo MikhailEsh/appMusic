f7aa860542a1507806f2a1cab8ced58ed0a3aec0
MikhailEsh <eshtokin.o@yandex.ru>
2016-04-13 21:52:30 +0300

    Добавлен Scroll и настроена кнопка Back

--- musicApp/app/src/main/java/com/main/hubluzar/musicapp/GroupMusicActivity.java
+++ musicApp/app/src/main/java/com/main/hubluzar/musicapp/GroupMusicActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -23,15 +25,24 @@ public class GroupMusicActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_main);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        //getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         Intent intent = getIntent();
         setContentActivityText(intent);
 
         ImageLoader imageLoader = createImageLoader();
         setContentActivityImage(imageLoader, intent);
+    }
 
-
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private void setContentActivityText(Intent intent)
@@ -39,6 +50,7 @@ public class GroupMusicActivity extends AppCompatActivity {
         getSupportActionBar().setTitle(intent.getStringExtra(getString(R.string.common_labelGroup_name)));
 
         TextView descriptionGroup = (TextView) findViewById(R.id.group_textView_Description);
+        descriptionGroup.setMovementMethod(new ScrollingMovementMethod());
         descriptionGroup.setText(intent.getStringExtra(getString(R.string.common_labelGroup_description)));
     }
     private void setContentActivityImage(ImageLoader imageLoader, Intent intent)
